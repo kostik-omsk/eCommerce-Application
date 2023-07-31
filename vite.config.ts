bb5a63863bf3fcd74f6e6c8cd7c26dcead06b6e1
HunterTigerX <HunterTigerX@gmail.com>
2023-08-01 01:12:38 +0300

    add vite support for both development and production build modes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    return {
+      mode: 'production',
+      plugins: [react()],
+    };
+  } else if (command === 'development'){
+    return {
+      mode: 'dev',
+      plugins: [react()],
+    };
+  }
 });
