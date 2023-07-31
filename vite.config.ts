e1e1a74ed6bc391cb25623e2747e4d1b3b72b8e2
HunterTigerX <HunterTigerX@gmail.com>
2023-08-01 01:21:46 +0300

    refractor: format code by linter

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  if (command === 'build') {
+  if (command === "build") {
     return {
-      mode: 'production',
+      mode: "production",
       plugins: [react()],
     };
-  } else if (command === 'development'){
+  } else if (command === "development") {
     return {
-      mode: 'dev',
+      mode: "dev",
       plugins: [react()],
     };
   }
