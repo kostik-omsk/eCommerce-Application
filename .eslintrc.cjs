6f29f2e7b950f29c3348936975136700b4cf85cb
HunterTigerX <HunterTigerX@gmail.com>
2023-08-01 20:16:34 +0300

    fix: merge two eslint configs to one. extended config files. moved vite dependencies from dev to dependencies. add react version settings to eslint config. fixed linter errors in code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "airbnb-typescript/base",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+  },
+  plugins: ["react-refresh", "react", "import"],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
